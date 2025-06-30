9da12403d677ec78368505844f4b8972d904855d
Tashi Phuntsho <phuntsho931@gmail.com>
2025-06-30 12:48:54 +0600

    check if its an even word

--- fnc.js
+++ fnc.js
@@ -1,15 +1,15 @@
 // console.log("hey");
-// function reverseString(wrd) {
-//   let res = "";
-//   for (let i = wrd.length - 1; i >= 0; i-=1) {
-//     res += wrd[i];
-//   }
-//   return res;
-// }
+function reverseString(wrd) {
+  let res = "";
+  for (let i = wrd.length - 1; i >= 0; i-=1) {
+    res += wrd[i];
+  }
+  return res;
+}
 // console.log(reverseString("hello"))
 
 function replace(str, charToReplace, strReplacement){
-    let res ="";
+    let res1 ="";
     for(let i = 0; i< str.length; i+=1){
         if(str[i] === charToReplace){
             res += strReplacement;
@@ -19,5 +19,14 @@ function replace(str, charToReplace, strReplacement){
  }
 return res;
 }
-console.log(replace("hello","l", "sad"));
+// console.log(replace("hello","l", "ff"));
 
+function strLengthEven(str1) {
+    if (str1.length % 2 === 0) {
+        return "even";
+    } else {
+        return "not even";
+    }
+}
+
+console.log(strLengthEven("hell"));
