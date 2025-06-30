fa6e99727b536057e16d243370d9feedd9d52c7a
Tashi Phuntsho <phuntsho931@gmail.com>
2025-06-30 12:37:58 +0600

    replacing letter in a word

--- fnc.js
+++ fnc.js
@@ -1,9 +1,23 @@
 // console.log("hey");
-function reverseString(wrd) {
-  let res = "";
-  for (let i = wrd.length - 1; i >= 0; i-=1) {
-    res += wrd[i];
-  }
-  return res;
+// function reverseString(wrd) {
+//   let res = "";
+//   for (let i = wrd.length - 1; i >= 0; i-=1) {
+//     res += wrd[i];
+//   }
+//   return res;
+// }
+// console.log(reverseString("hello"))
+
+function replace(str, charToReplace, strReplacement){
+    let res ="";
+    for(let i = 0; i< str.length; i+=1){
+        if(str[i] === charToReplace){
+            res += strReplacement;
+    } else{
+        res += str[i];
+    }
+ }
+return res;
 }
-console.log(reverseString("hello"))
+console.log(replace("hello","l", "sad"));
+
